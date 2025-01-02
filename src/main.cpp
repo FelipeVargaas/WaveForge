5df496147629253af3d80adb417e4972d64aed72
Felipe Vargas <felipevargasrj@gmail.com>
2025-01-02 16:04:47 -0300

    Iniciando comunicação Serial via BT

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <Arduino.h>
-#include "wave_generator.h"
+#include "global.h"
 #include "pins.h"
+#include "wave_generator.h"
+#include <BluetoothSerial.h> // Biblioteca para Bluetooth        
 
+BluetoothSerial SerialBT; // Objeto para Bluetooth
 #define PWM_RESOLUTION 8 // Resolução de 8 bits (0-255)
-//const int pwmResolution = 8; // Resolução de 8 bits (0-255)
-const int DUTY_50 = 50; // Resolução de 8 bits (0-255)
+void processCommand(const String &command);
+String receivedCommand;   // String para armazenar o comando recebido
+
 
 void setup() {
+  Serial.begin(115200);  // Inicializa o Serial para debugging
+  SerialBT.begin("WaveForge"); // Nome do dispositivo Bluetooth
+
   // Configuração do PWM
   setupPWM(PIN_RPM, CHANNEL_RPM, PWM_RESOLUTION);
   setupPWM(PIN_INJECTOR_A, CHANNEL_INJECTOR_A, PWM_RESOLUTION);
@@ -35,16 +42,26 @@ void setup() {
 }
 
 void loop() {
-    setPWM(CHANNEL_RPM, 5000, DUTY_50);
-    setPWM(CHANNEL_INJECTOR_A, 5000, DUTY_50);
-    setPWM(CHANNEL_INJECTOR_B, 5000, DUTY_50);
-    setPWM(CHANNEL_INJECTOR_C, 5000, DUTY_50);
-    setPWM(CHANNEL_INJECTOR_D, 5000, DUTY_50);
-    setPWM(CHANNEL_MANOMETER, 5000, DUTY_50);
-    setPWM(CHANNEL_MAP_REDUCTOR, 5000, DUTY_50);
-    setPWM(CHANNEL_MAP_COLECTOR, 5000, DUTY_50);
-    setPWM(CHANNEL_TEMP_GNV, 5000, DUTY_50);
-    setPWM(CHANNEL_HPS, 5000, DUTY_50);
-    setPWM(CHANNEL_LPS, 5000, DUTY_50);
-    delay(500);
+  // Verifica se há comandos recebidos via Bluetooth
+  if (SerialBT.available()) {
+    char incomingChar = SerialBT.read();
+    if (incomingChar == '\n') {
+      processCommand(receivedCommand); // Processa o comando completo
+      receivedCommand = "";            // Limpa a string
+    } else {
+      receivedCommand += incomingChar; // Adiciona o caractere recebido
+    }
+  }
+  delay(50); // Pequeno delay para evitar uso excessivo de CPU
+}
+
+// Função para processar os comandos recebidos via Bluetooth
+void processCommand(const String &command) {
+  int channel, freq, duty;
+  if (sscanf(command.c_str(), "%d,%d,%d", &channel, &freq, &duty) == 3) {
+    setPWM(channel, freq, duty);
+    SerialBT.printf("Channel: %d, Frequency: %d Hz, Duty: %d%%\n", channel, freq, duty);
+  } else {
+    SerialBT.println("Invalid command. Use format: channel,freq,duty");
+  }
 }
